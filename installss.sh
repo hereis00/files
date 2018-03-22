0b9b01cac61a4a8ddfde25a9a1159fe697598169
hereis00 <982025911@qq.com>
2018-03-22 21:17:53 +0800

    add shadowsocks script

---
+++ installss.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+# Install Shadowsocks on CentOS 7
+
+echo "Installing Shadowsocks..."
+
+random-string()
+{
+    cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w ${1:-32} | head -n 1
+}
+
+CONFIG_FILE=/etc/shadowsocks.json
+SERVICE_FILE=/etc/systemd/system/shadowsocks.service
+SS_PASSWORD="shadowsocks.00"
+SS_PORT=8388
+SS_METHOD=aes-256-cfb
+SS_IP=`ip route get 1 | awk '{print $NF;exit}'`
+GET_PIP_FILE=/tmp/get-pip.py
+
+# install pip
+curl "https://bootstrap.pypa.io/get-pip.py" -o "${GET_PIP_FILE}"
+python ${GET_PIP_FILE}
+
+# install shadowsocks
+pip install --upgrade pip
+pip install shadowsocks
+
+# create shadowsocls config
+cat <<EOF | tee ${CONFIG_FILE}
+{
+  "server": "0.0.0.0",
+  "server_port": ${SS_PORT},
+  "password": "${SS_PASSWORD}",
+  "method": "${SS_METHOD}"
+}
+EOF
+
+# create service
+cat <<EOF | tee ${SERVICE_FILE}
+[Unit]
+Description=Shadowsocks
+
+[Service]
+TimeoutStartSec=0
+ExecStart=/usr/bin/ssserver -c ${CONFIG_FILE}
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# start service
+systemctl enable shadowsocks
+systemctl start shadowsocks
+
+# start firewall
+firewall-cmd --zone=public --add-port=8388/tcp permanent
+firewall-cmd --reload
+
+# view service status
+sleep 5
+systemctl status shadowsocks -l
+
+#view firewall status
+sleep 5
+systemctl status firewalld
+echo "if firewall not running,run by systemctl start firewalld"
+
+echo "================================"
+echo ""
+echo "Congratulations! Shadowsocks has been installed on your system."
+echo "You shadowsocks connection info:"
+echo "--------------------------------"
+echo "server:      ${SS_IP}"
+echo "server_port: ${SS_PORT}"
+echo "password:    ${SS_PASSWORD}"
+echo "method:      ${SS_METHOD}"
+echo "--------------------------------"
+
+echo "need bbr?(y/n):"
+yes="y"
+Yes="Y"
+read confirm
+if [[ ${confirm:0:1} = $yes || ${config:0:1} = $Yes ]]
+then
+yum -y install wget
+wget --no-check-certificate https://github.com/teddysun/across/raw/master/bbr.sh
+chmod +x bbr.sh
+./bbr.sh
+fi
